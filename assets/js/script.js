bc066891d792b0d7d527c5f8e497d577e974118b
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-07-22 09:42:07 +0000

    harryQuestions variable updated with questions and multiple choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,56 @@
-let harryQuestions
+let harryQuestions = [
+    {
+        question: "What house does Harry belong to?",
+        choices: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+        correct: 0
+    },
+    {
+        question: "What position does Harry Play in Quidditch?",
+        choices: ["Beater", "Seeker", "Keeper", "Chaser"],
+        correct: 1
+    },
+    {
+        question: "What kind of wood is Harry's first wand made out of?",
+        choices: ["Oak", "Willow", "Beech", "Holly"],
+        correct: 3
+    },
+    {
+        question: "What is Harry's middle name?",
+        choices: ["James", "Lupin", "Sirius", "Tom"],
+        correct: 0
+    },
+    {
+        question: "What type of animal does Harry accidentally set free while at the zoo in Harry Potter and the Philosopher's Stone?",
+        choices: ["Lion", "Rat", "Snake", "Spider"],
+        correct: 2 
+    },
+    {
+        question: "Where was Harry Potter born?",
+        choices: ["The Burrow", "Godric's Hollow", "Little Whinging", "Number 12 Grimmauld Place"],
+        correct: 1
+    },
+    {
+        question: "Who is Harry Potter's Godfather?",
+        choices: ["Peter Petigrew", "Remus Lupin", "Sirius Black", "Albus Dumbledor"],
+        correct: 2
+    },
+    {
+        question: "What is the name of Harry's owl?",
+        choices: ["Fluffy", "Padfort", "Crookshanks", "Hedwig"],
+        correct: 3
+    },
+    {
+        question: "What fake name does Harry use while attending Bill and Fleur's wedding in disguise in Harry Potter and the Deathly Hallows?",
+        choices: ["Barny Weasly", "Bert Weasly", "Bagshot Weasly", "Billy Weasly"],
+        correct: 0 
+    },
+    {
+        question: "When is Harry's birthday?",
+        choices: ["June 31st","July 31st", "August 31st", "September 31st"],
+        correct: 1
+    },
+    
+];
 
 let ronQuestions
 
