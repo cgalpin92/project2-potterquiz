f5065b77c09289561a80c62ca67849a70f341b69
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-07-23 14:17:14 +0000

    checkAnswers function created for each set of questions. event listener added to choices buttons to run checkAnswers function when button selected. fixed bug where wrong answer being returned each time, changed correct value to a string so that console reads both choice and correct as equal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,52 +3,52 @@
 let harryQuestions = [{
         question: "What house does Harry belong to?",
         choices: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
-        correct: 0
+        correct: "Gryffindor"
     },
     {
         question: "What position does Harry Play in Quidditch?",
         choices: ["Beater", "Seeker", "Keeper", "Chaser"],
-        correct: 1
+        correct: "Seeker"
     },
     {
         question: "What kind of wood is Harry's first wand made out of?",
         choices: ["Oak", "Willow", "Beech", "Holly"],
-        correct: 3
+        correct: "Holly"
     },
     {
         question: "What is Harry's middle name?",
         choices: ["James", "Lupin", "Sirius", "Tom"],
-        correct: 0
+        correct: "James"
     },
     {
         question: "What type of animal does Harry accidentally set free while at the zoo in Harry Potter and the Philosopher's Stone?",
         choices: ["Lion", "Rat", "Snake", "Spider"],
-        correct: 2
+        correct: "Snake"
     },
     {
         question: "Where was Harry Potter born?",
         choices: ["The Burrow", "Godric's Hollow", "Little Whinging", "Number 12 Grimmauld Place"],
-        correct: 1
+        correct: "Godric's Hollow"
     },
     {
         question: "Who is Harry Potter's Godfather?",
         choices: ["Peter Petigrew", "Remus Lupin", "Sirius Black", "Albus Dumbledore"],
-        correct: 2
+        correct: "Sirius Black"
     },
     {
         question: "What is the name of Harry's owl?",
         choices: ["Fluffy", "Padfort", "Crookshanks", "Hedwig"],
-        correct: 3
+        correct: "Hedwig"
     },
     {
         question: "What fake name does Harry use while attending Bill and Fleur's wedding in disguise in Harry Potter and the Deathly Hallows?",
         choices: ["Barny Weasly", "Bert Weasly", "Bagshot Weasly", "Billy Weasly"],
-        correct: 0
+        correct: "Barny Weasly"
     },
     {
         question: "When is Harry's birthday?",
         choices: ["June 31st", "July 31st", "August 31st", "September 31st"],
-        correct: 1
+        correct: "July 31st"
     },
 
 ];
@@ -56,52 +56,52 @@ let harryQuestions = [{
 let ronQuestions = [{
         question: "What is the name of the Weasley family home?",
         choices: ["Number 12 Grimmauld Place", "Little Whinging", "The Burrow", "Godric's Hollow"],
-        correct: 2
+        correct: "The Burrow"
     },
     {
         question: "What is Ron famously afraid of?",
         choices: ["Rats", "Spiders", "Dragons", "Snakes"],
-        correct: 1
+        correct: "Spiders"
     },
     {
         question: "What is Ron's middle name?",
         choices: ["Arthur", "Barney", "Bilius", "Arnold"],
-        correct: 2
+        correct: "Bilius"
     },
     {
         question: "What position does Ron play in Quidditch?",
         choices: ["Seeker", "Beater", "Chaser", "Keeper"],
-        correct: 3
+        correct: "Keeper"
     },
     {
         question: "Which Weasley dies during the final battle of Hogwarts?",
         choices: ["Bill", "Fred", "George", "Ron"],
-        correct: 1
+        correct: "Fred"
     },
     {
         question: "What is the first name of Ron's mother Mrs Weasley",
         choices: ["Molly", "Tonks", "Lilly", "Minerva"],
-        correct: 0
+        correct: "Molly"
     },
     {
         question: "What does Ginny say Ron has a tattoo of?",
         choices: ["A chess piece Knight", "A Lion", "A Dragon", "A Pygmy Puff"],
-        correct: 3
+        correct: "A Pygmy Puff"
     },
     {
         question: "What is Ron's favourite Quidditch team?",
         choices: ["Chudley Cannons", "Holyhead Harpies", "Puddlemere United", "Tutshill Tornados"],
-        correct: 0
+        correct: "Chudley Cannons"
     },
     {
         question: "What is the name of Ron's pet owl?",
         choices: ["Fang", "Trevor", "Scabbers", "Pigwidgeon"],
-        correct: 3
+        correct: "Pigwidgeon"
     },
     {
         question: "What was the name of Ron's first girlfriend?",
         choices: ["Luna Lovegood", "Lavender Brown", "Cho Chang", "Padma Patil"],
-        correct: 1
+        correct: "Lavender Brown"
     },
 
 ];
@@ -110,52 +110,52 @@ let ronQuestions = [{
 let hermioneQuestions = [{
         question: "What is Hermione's first line in the first movie?",
         choices: ["Has anyone seen a toad?", "Oh, are you doing magic?", "You've got dirt on your nose, by the way", "I'm Hermione Granger"],
-        correct: 0
+        correct: "Has anyone seen a toad?"
     },
     {
         question: "What are Hermione's parents?",
         choices: ["Doctors", "Dentists", "Teachers", "Veternarians"],
-        correct: 1
+        correct: "Dentists"
     },
     {
         question: "Who takes Hermione to the Yule Ball",
         choices: ["Harry Potter", "Neville Longbottom", "Fred Weasly", "Viktor Krum"],
-        correct: 3
+        correct: "Viktor Krum"
     },
     {
         question: "What is Hermione's middle name?",
         choices: ["Jean", "Bellatrix", "Lilly", "Lavender"],
-        correct: 0
+        correct: "Jean"
     },
     {
         question: "What is Hermione's cat called?",
         choices: ["Fluffy", "Padfort", "Crookshanks", "Fang"],
-        correct: 2
+        correct: "Crookshanks"
     },
     {
         question: "What is Hermione's patronus?",
         choices: ["Doe", "Otter", "Cat", "Swan"],
-        correct: 1
+        correct: "Otter"
     },
     {
         question: "What is Hermione's wand core?",
         choices: ["Pheonix feather", "Unicorn hair", "Ownl feather", "Dragon heartstring"],
-        correct: 3
+        correct: "Dragon heartstring"
     },
     {
         question: "Why did Hermione miss the lesson on Cheering Charms?",
         choices: ["She was stunned by a Basilisk", "She had turned into a cat after taking polyjuice potion", "Draco's cruel bahaviour made her upset", "She ate too many puking pastels"],
-        correct: 2
+        correct: "Draco's cruel bahaviour made her upset"
     },
     {
         question: "Where did Hermione hold the first meeting for students interest in joining Dumbledore's Army?",
         choices: ["The Hog's Head", "The Leaky Cauldron", "The Shrieking Shack", "The Hogwarts Library"],
-        correct: 0
+        correct: "The Hog's Head"
     },
     {
         question: "What are Hermione's favourite subjects?",
         choices: ["Transfiguration and Charms", "Herbology and Arithmancy", "Transfiguration and Arithmancy", "Arithmancy and Charms"],
-        correct: 3
+        correct: "Arithmancy and Charms"
     },
 
 ];
@@ -234,10 +234,11 @@ function displayHarryQuestion() {
     displayQuestion.choices.forEach((choice, index) => {
         let input = document.createElement('BUTTON');
         input.type = "radio";
-        input.setAttribute('onclick', 'nextQuestionOne();')
+        //input.setAttribute('onclick', 'nextQuestionOne();')
         input.value = "index";
         input.textContent = choice;
         input.setAttribute('class', 'button-choices');
+        input.addEventListener('click', () => checkHarryAnswer(choice));
         choicesElement.appendChild(input);
 
         //let label = document.createElement('label');
@@ -253,6 +254,18 @@ function displayHarryQuestion() {
     quizArea.appendChild(btnTwo);
 
 
+};
+
+function checkHarryAnswer(choice) {
+    console.log('choice: ', choice);
+    let correct = harryQuestions[currentQuestion].correct;
+    console.log('correct: ', correct);
+    if (choice === correct) {
+        alert('correct');
+    } else {
+        alert('wrong');
+    }
+    nextQuestionOne();
 };
 
 function nextQuestionOne() {
@@ -290,11 +303,12 @@ function displayRonQuestion() {
     choicesElement.innerHTML = "";
     displayQuestion.choices.forEach((choice, index) => {
         let input = document.createElement('BUTTON');
-        input.type = "radio";
-        input.setAttribute('onclick', 'nextQuestionTwo();')
+        //input.setAttribute('onclick', 'checkAnswer();')
         input.value = "index";
         input.textContent = choice;
+        console.log('choice: ', choice)
         input.setAttribute('class', 'button-choices');
+        input.addEventListener('click', () => checkRonAnswer(choice));
         choicesElement.appendChild(input);
     });
 
@@ -306,6 +320,24 @@ function displayRonQuestion() {
     quizArea.appendChild(btnTwo);
 };
 
+function checkRonAnswer(choice) {
+    //let choices = ronQuestions[currentQuestion].choices;
+    //console.log('choices: ', choices);
+    //let displayQuestion = ronQuestions[currentQuestion];
+    //console.log('displayQuestion: ', displayQuestion);
+    //let value = ronQuestions[currentQuestion].choices.index;
+    //console.log('value: ', value);
+    console.log('choice: ', choice);
+    let correct = ronQuestions[currentQuestion].correct;
+    console.log('correct: ', correct);
+    if (choice === correct) {
+        alert('correct');
+    } else {
+        alert('wrong');
+    }
+    nextQuestionTwo();
+};
+
 function nextQuestionTwo() {
     currentQuestion++
     if (currentQuestion < ronQuestions.length) {
@@ -315,6 +347,8 @@ function nextQuestionTwo() {
         currentQuestion = 0;
     }
 };
+
+
 
 function runRonGame() {
     hideMainContent();
@@ -339,10 +373,11 @@ function displayHermioneQuestion() {
     displayQuestion.choices.forEach((choice, index) => {
         let input = document.createElement('BUTTON');
         input.type = "radio";
-        input.setAttribute('onclick', 'nextQuestionThree();')
+        //input.setAttribute('onclick', 'nextQuestionThree();')
         input.value = "index";
         input.textContent = choice;
         input.setAttribute('class', 'button-choices');
+        input.addEventListener('click', () => checkHermioneAnswer(choice));
         choicesElement.appendChild(input);
     });
 
@@ -352,6 +387,18 @@ function displayHermioneQuestion() {
     btnTwo.textContent = "home";
     //quizArea.appendChild(btnOne);
     quizArea.appendChild(btnTwo);
+};
+
+function checkHermioneAnswer(choice) {
+    console.log('choice: ', choice);
+    let correct = hermioneQuestions[currentQuestion].correct;
+    console.log('correct: ', correct);
+    if (choice === correct) {
+        alert('correct');
+    } else {
+        alert('wrong');
+    }
+    nextQuestionThree();
 };
 
 function nextQuestionThree() {
